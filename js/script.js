adad502ae5adcd56b4c2b999539c3a13f10042cd
soyedarif <soyedariff@gmail.com>
2023-02-19 22:19:55 +0600

    function expression

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-'use strict';
+"use strict";
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnClose = document.querySelector(".close-modal");
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const butttons = document.querySelectorAll(".show-modal");
+butttons.forEach(button => {
+  button.addEventListener("click", openModal);
+});
+btnClose.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
